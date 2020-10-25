d0fa346f409ff58c07bf55a8a44057f9a23c0c8c
unknown <xmfeng95@hotmail.com>
2020-10-25 05:45:12 -1200

    hello

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,11 +19,11 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li>Modified by MenGY ♥️</li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
+              
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,43 +1,92 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Portfolio extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      life:[],
+    };
+    //this.handleShowDialog = this.handleShowDialog.bind(this);
+  }
+  handleShowDialog = () => {
+    this.setState({ isOpen: true });
+    console.log("cliked");
+  };
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
+  render() {
+    const {life} = this.state;
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = "images/portfolio/" + projects.image;
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={projects.url} title={projects.title}>
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+                {/* <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div> */}
+              </a>
+            </div>
           </div>
-        </div>
-      })
+        );
+      });
+      var lifeStyle = this.props.data.lifeStyle.map(function (lifeStyle) {
+        var lifeImage = "images/portfolio/" + lifeStyle.image;
+        
+        return (
+          <div key={lifeStyle.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={lifeStyle.url} title={lifeStyle.title}>
+                <img alt={lifeStyle.title} src={lifeImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{lifeStyle.title}</h5>
+                    <p>{lifeStyle.category}</p>
+                  </div>
+                </div>
+                {/* <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div> */}
+              </a>
+            </div>
+          </div>
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
+            <h4>Fashion</h4>
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
+            </div>
+            <h4 style={{ textAlign: "right" }}>Life Style</h4>
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+              onClick={this.handleShowDialog}
+            >
+              {lifeStyle}
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+            
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
